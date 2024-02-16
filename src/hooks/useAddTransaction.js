d81cd37c52d504594d4af5e075596c8c77180211
thatfireboi <gcf2007@hotmail.com>
2024-02-15 20:27:25 -0400

    Corrections to constant in function and commenting on imports brought from Firebase

--- src/hooks/useAddTransaction.js
+++ src/hooks/useAddTransaction.js
@@ -1,7 +1,8 @@
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import { db } from "../config/firebase-config";
 import { useGetUserInfo } from "./useGetUserInfo";
-
+// From the useAddTransaction.js file, we are importing the addDoc, collection, serverTimestamp, and db from the firebase/firestore and 
+// config/firebase-config files. We are also importing the useGetUserInfo hook from the useGetUserInfo file.
 export const useAddTransaction = () => {
   const transactionCollectionRef = collection(db, "transactions");
   const { userID } = useGetUserInfo();
