99a9bbf4d9ffebfb2fd0db65be248bcfc48da5a6
thatfireboi <gcf2007@hotmail.com>
2024-02-27 20:48:10 -0400

    omported budget delete hook and added delete icon

--- src/pages/budgets/BudgetList.jsx
+++ src/pages/budgets/BudgetList.jsx
@@ -1,27 +1,38 @@
 import React from "react";
 import { useGetBudgets } from "../../hooks/useGetBudgets";
+import { useDeleteBudget } from "../../hooks/useDeleteBudget";
 import { ProgressBar } from "./ProgressBar";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { IconButton } from "@material-ui/core";
 
 export const BudgetList = ({ userID }) => {
   const budgets = useGetBudgets(userID);
+  const { deleteBudget } = useDeleteBudget();
+
+  const handleDeleteBudget = async (budgetID) => {
+      await deleteBudget(budgetID);
+  };
 
   return (
     <div className="budget-list">
       <h2>Budgets</h2>
       {budgets.length > 0 ? (
         <ul>
-          {budgets.map((budget) => (
-            <li key={budget.id} className="budget-item">
-              <p><b>{budget.name}|</b></p>
-              <p><u>Target Amount</u>: ${budget.targetAmount}</p>
-              <p><u>Current Amount</u>: ${budget.currentAmount}</p>
-              <ProgressBar currentAmount={budget.currentAmount} targetAmount={budget.targetAmount} />
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>No budgets found.</p>
-      )}
+              {budgets.map((budget) => (
+                <li key={budget.id} className="budget-item">
+                  <p><b>{budget.name}|</b></p>
+                  <p><u>Target Amount</u>: ${budget.targetAmount}</p>
+                  <p><u>Current Amount</u>: ${budget.currentAmount}</p>
+                  <ProgressBar currentAmount={budget.currentAmount} targetAmount={budget.targetAmount} />
+                  <IconButton onClick={() => handleDeleteBudget(budget.id)} aria-label="delete" style={{ color: 'red'}}>
+                    <DeleteIcon />
+                  </IconButton>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No budgets found.</p>
+          )}
     </div>
   );
 };
