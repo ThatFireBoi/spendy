efcf1b4e4eac88c9487f40cce48fe9c2a06479d1
thatfireboi <gcf2007@hotmail.com>
2024-02-27 21:19:15 -0400

    Toast added for when budget is deleted

--- src/hooks/useDeleteBudget.js
+++ src/hooks/useDeleteBudget.js
@@ -1,11 +1,19 @@
 import { doc, deleteDoc } from "firebase/firestore";
 import { db } from "../config/firebase-config";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export const useDeleteBudget = () => {
   const deleteBudget = async (budgetID) => {
     const budgetRef = doc(db, "budgets", budgetID);
     await deleteDoc(budgetRef);
-  };
+    toast('Budget deleted successfully!', {
+        style: {
+            backgroundColor: 'red',
+            color: 'white'
+        }
+  });
+};
 
   return { deleteBudget };
 };
