01611c75108a16b75a69833ef6d7888021be541a
thatfireboi <gcf2007@hotmail.com>
2024-02-28 10:53:24 -0400

    Corrections to typos

--- src/hooks/useAddTransaction.js
+++ src/hooks/useAddTransaction.js
@@ -1,8 +1,7 @@
 import { addDoc, collection, serverTimestamp, doc, updateDoc, getDoc } from "firebase/firestore";
 import { db } from "../config/firebase-config";
 import { useGetUserInfo } from "./useGetUserInfo";
-// From the useAddTransaction.js file, we are importing the addDoc, collection, serverTimestamp, and db from the firebase/firestore and 
-// config/firebase-config files. We are also importing the useGetUserInfo hook from the useGetUserInfo file.
+
 export const useAddTransaction = () => {
   const transactionCollectionRef = collection(db, "transactions");
   const { userID } = useGetUserInfo();
@@ -21,7 +20,7 @@ export const useAddTransaction = () => {
           createdAt: serverTimestamp(),
       });
         if (transactionType === 'income' && budgetID) {
-          const budgetRef = doc(db, 'budgets', budgetID); // Assuming you have a budgetID
+          const budgetRef = doc(db, 'budgets', budgetID);
           getDoc(budgetRef).then((docSnap) => {
             if (docSnap.exists()) {
               const newCurrentAmount = Number(docSnap.data().currentAmount) + amountNumber;
