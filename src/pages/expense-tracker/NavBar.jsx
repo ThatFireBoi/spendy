98d0cf11b74f44d2fa452b27a40c449ec99f97ee
nataliagrivera <nataliagirvera62@gmail.com>
2024-03-07 10:33:37 -0400

    NavBaar changes so far.

--- src/pages/expense-tracker/NavBar.jsx
+++ src/pages/expense-tracker/NavBar.jsx
@@ -1,22 +1,57 @@
 import React from "react";
-// NavBar icons, material-ui
-import PaidIcon from '@mui/icons-material/Paid';
-import ReceiptIcon from '@mui/icons-material/Receipt';
-import EmojiEventsIcon from "@material-ui/icons/EmojiEvents";
+// Remove unused import {ComponentName} from '@material-tailwind/react';
+
+// Remove unused imports
+// import PaidIcon from '@mui/icons-material/Paid';
+// import ReceiptIcon from '@mui/icons-material/Receipt';
+// import EmojiEventsIcon from "@material-ui/icons/EmojiEvents";
 import "./NavBar.css";
+
 import logo from "./spendyLogo.png";
 import { Link } from "react-router-dom";
 
+import {
+    Card,
+    Typography,
+    List,
+    ListItem,
+    ListItemPrefix,
+} from "@material-tailwind/react";
+import {
+    ReceiptPercentIcon,
+    TrophyIcon,
+    UserCircleIcon,
+} from "@heroicons/react/24/solid";
 
-export const NavBar = () => {
+
+export function NavBar() {
     return (
-        <nav className="nav-bar">
-            <img src={logo} alt="logo" className="nav-logo"/>
-            <ul>
-                <li><PaidIcon /><a href="/expense-tracker">Transactions</a></li>
-                <li><ReceiptIcon /><Link to="/expense-tracker">Receipts</Link></li>
-                <li><EmojiEventsIcon /><a href="/expense-tracker">Achievements</a></li>
-            </ul>
-        </nav>
+        <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
+            <div className="navbar logo">
+                <Typography variant="h5" color="blue-gray">
+                    SPENDY
+                </Typography>
+            </div>
+            <List>
+                <ListItem>
+                    <ListItemPrefix>
+                        <ReceiptPercentIcon className="nav-logo" />
+                    </ListItemPrefix>
+                    Transactions & Receipts
+                </ListItem>
+                <ListItem>
+                    <ListItemPrefix>
+                        <TrophyIcon className="nav-logo" />
+                    </ListItemPrefix>
+                    Achievements
+                </ListItem>
+                <ListItem>
+                    <ListItemPrefix>
+                        <UserCircleIcon className="nav-logo" />
+                    </ListItemPrefix>
+                    Profile
+                </ListItem>
+            </List>
+        </Card>
     );
-};
+}
