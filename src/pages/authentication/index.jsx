9740799b7242e70c5694bfe367c0ebbca5fbc9cd
nataliagrivera <nataliagirvera62@gmail.com>
2024-03-15 11:24:23 -0400

    Add headshots and LinkedIn/GitHub links to About Us section

--- src/pages/authentication/index.jsx
+++ src/pages/authentication/index.jsx
@@ -2,6 +2,8 @@ import { auth, provider } from "../../config/firebase-config";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 import "./styles.css";
+import headshot from "./Gabriels_profesional_shots.jpeg";
+import headshot2 from "./Natalia_profesional_shot.jpeg";
 
 const Header = () => {
     return (
@@ -61,14 +63,23 @@ const AboutUs = () => {
         <div className="about-us" id="about-us">
             <h2><span class="fancy">About us</span></h2>
             <p>
-            We're Natalia Rivera and Gabriel Castro, two aspiring developers excited to embark on this journey in the world of tech.
+            We're Gabriel Castro and Natalia Rivera, two aspiring developers excited to embark on this journey in the world of tech.
             As newcomers to the realm of software development, we're eager to learn, grow, and make a positive impact through our work. With a passion for problem-solving and a commitment to continuous improvement, we're diving headfirst into the world of coding, ready to tackle any challenge that comes our way.
             </p>
             <p>
             we're always open to feedback, suggestions, and new connections. If you'd like to learn more about us or connect with us further, feel free to check out our GitHub and LinkedIn profiles:
             </p>
             <div>
-            <img src="/home/nataliagrivera/Holberton Group Projects/spendy/public/Gabriels_profesional_shots.jpeg"></img>
+            <img src={headshot} alt="headshot" className="headshot"/>
+            <img src={headshot2} alt="headshot" className="headshot"/>
+            </div>
+            <div>
+                <a href="https://www.linkedin.com/in/gabrielcastroflores/" class="button-linkedin" >Gabriel's LinkedIn</a>
+                <a href="https://www.linkedin.com/in/nataliagrivera/" class="button-linkedin">Natalia's LinkedIn</a>
+            </div>
+            <div className="github links">
+                <a href="https://github.com/ThatFireBoi" class="button-git">Gabriel's GitHub</a>
+                <a href="https://github.com/nataliagrivera" class="button-git">Natalia's GitHub</a>
             </div>
         </div>
     );
